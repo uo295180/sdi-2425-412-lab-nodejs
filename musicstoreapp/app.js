1add1926bbc113ec40c5ffde57c6d9c0cb87500d
orvizz <marioorvvie@gmail.com>
2025-03-10 10:52:14 +0100

    SDI-2425-412-8.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -3,24 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-require("./routes/songs.js")(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+require("./routes/songs.js")(app);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
