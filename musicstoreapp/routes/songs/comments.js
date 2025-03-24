e6442f773f2d78529972b8cef3c28463b62f3c89
orvizz <marioorvvie@gmail.com>
2025-03-24 16:40:49 +0100

    SDI-2425-412-10.2-Redirección de peticiones.

--- musicstoreapp/routes/songs/comments.js
+++ musicstoreapp/routes/songs/comments.js
@@ -4,17 +4,12 @@ module.exports = function (app, commentsRepository) {
         let filter = {_id: new ObjectId(req.body.song_id)};
 
         commentsRepository.addComments(favoriteSong, function (result) {
-                if(result.error != null) {
-                    res.send("Error al añadir canción a favoritos " + result.error);
-                } else {
-                    res.redirect("/songs/favorites");
-                }
-            });
-        }).catch(error => {
-            res.send("Se ha producido un error al recuperar la canción " + error);
-        })
-
-
+            if(result.error != null) {
+                res.send("Error al añadir canción a favoritos " + result.error);
+            } else {
+                res.redirect("/songs/favorites");
+            }
+        });
     });
 
 }
