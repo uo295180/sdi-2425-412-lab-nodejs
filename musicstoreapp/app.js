2d60c9d6696aacba830f2be9e8aa3df7e6f8b46a
orvizz <marioorvvie@gmail.com>
2025-04-01 17:49:25 +0200

    SDI-2425-412-11.3-SW Cliente JQuery + AJAX.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -20,6 +20,15 @@ app.set('jwt', jwt)
 
 let crypto = require('crypto');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,  Accept, token");
+// Debemos especificar todas las headers que se aceptan. Content-Type , token
+  next();
+});
+
 let fileUpload = require('express-fileupload');
 app.use(fileUpload({
   limits: {fileSize: 50*1024*1024},
